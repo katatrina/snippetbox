bbfd67fa1c2e6f3b06cc9e5330e622db46319846
chauvinhphuoc <cvphuoc2014@gmail.com>
2023-08-15 13:17:32 +0700

    update helpers

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -7,12 +7,17 @@ import (
 	"time"
 )
 
-// serverError logs the error to stderr and writes internal server error into response.
+// serverError logs the error to stderr and writes Internal Server Error into response.
 func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
 	app.errorLog.Output(2, trace)
 
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+// clientError sends a specific status code and corresponding description to the user.
+func (app *application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
 }
 
 // humanDate returns a nicely formatted string representation
